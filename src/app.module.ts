5d51f95939aad6d54beef672a619f89a284fac06
HaehunLee <allman956@naver.com>
2022-11-10 00:10:36 +0900

    Feat: TypeOrmModule 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,8 @@ import { UsersModule } from './users/users.module';
 import { WorkspacesModule } from './workspaces/workspaces.module';
 import { ChannelsModule } from './channels/channels.module';
 import { DmsModule } from './dms/dms.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './common/decorators/user.decorator';
 
 // 서버에서 env값을 비동기로 가져와 적용할 수 있음.
 // const getEnv = async () => {
@@ -27,6 +29,21 @@ import { DmsModule } from './dms/dms.module';
     WorkspacesModule,
     ChannelsModule,
     DmsModule, // forRoot 등은 설정을 넣어줄 때
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      // entities: ['entities/*.js'],
+      autoLoadEntities: true, // bug 발생 주의 // TypeOrmModule.forFeature 에 entities 매핑
+      synchronize: false,
+      logging: 'all', // 로깅
+      keepConnectionAlive: true, // 서버 자동 재시작 시, DB 연결 유지
+      charset: 'utf8mb4', // for emoji
+    }),
+    TypeOrmModule.forFeature([User]),
   ],
   controllers: [AppController],
   providers: [AppService, ConfigService],
